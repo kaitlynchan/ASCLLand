91613e7440132a761a04e280f036a70ccb9acffa
kaitlynchan <ununoctium829@gmail.com>
2016-12-13 09:28:55 -0800

    stuff

--- src/ASCLLand.java
+++ src/ASCLLand.java
@@ -1,4 +1,21 @@
 
 public class ASCLLand {
+	private int position;
+	
+	public ASCLLand (int initialPosition){
+		this.position = initialPosition;
+	}
+	
 
+
+	public void setPosition(int moveSpaces){
+		this.position += moveSpaces;
+		if (this.position < 0){
+			this.position = 0;
+		}
+	}
+	
+	public int getPosition(){
+		return this.position;
+	}
 }
--- src/playGame.java
+++ src/playGame.java
@@ -1,39 +1,57 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class playGame {
-
+	
+	ASCLLand playerA = new ASCLLand (0);
+	ASCLLand playerB = new ASCLLand (0);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ASCLLand playerA = new ASCLLand();
-		ASCLLand playerB = new ASCLLand();
+		
 		
 		for (int i = 1; i <= 5; i++){
 			System.out.println(playGame.round(i));
 		}
-		System.out.println();
-		Scanner in = new Scanner(System.in);
-		String statement = in.nextLine();
 
 	}
 	
+	public int movePosition (int roll){
+		playerA.getPosition();
+		System.out.println(playerA.getPosition());
+		//4 or 6
+		//other player at same position
+		return roll;
+	}
 
-
-	public static String round (int i){
-		System.out.println("Line #" + i + ": ");
+	public String round (int i){
+		System.out.print("Line #" + i + ": ");
 		Scanner userInput = new Scanner (System.in);
-		String diceRolls = userInput.nextLine();
-		String rollsArray [] = diceRolls.split(",");
-	
-		//for player A
-		for (int j = 0; j < rollsArray.length-2; k += 2){
-			
+		String diceRolls [] = (userInput.nextLine()).split(",");
+		int rollsArray [] = new int [diceRolls.length];
+		
+		for (int index = 0; index < rollsArray.length; index++){
+			rollsArray[index] = Integer.parseInt(diceRolls[index]);	
 		}
 		
-		//for player B
-		for (int k = 1; k < rollsArray.length-1; k += 2){
-			
+		System.out.println(Arrays.toString(rollsArray));
+	
+		for (int m = 0; m < rollsArray.length-1; m++){
+			if (m % 2 == 0){
+				playerA.setPosition(movePosition(rollsArray[m]));
+				//set position, based on movePosition
+				//playerA.setPosition(movePosition)
+			}
+			else {
+				//set position, based on movePosition
+				//playerB.setPosition(movePosition)
+			}
 		}
+		
 		return "A: , B:";
+		//get position for object A and B
+		//System.out.println(playerA.getPosition())
+		//System.out.println(playerB.getPosition())
 	}
 
 }
